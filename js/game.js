79e6fe1926f5b6eeab2d992317f35f3486a67472
Anikald <t1kend.offical@gmail.com>
2021-04-29 04:47:28 +1000

    настроил границы,  добавил корабли  и астероиды

--- js/game.js
+++ js/game.js
@@ -5,17 +5,18 @@ var canvas = document.getElementById('Game_js');
 var contex = canvas.getContext('2d');
 
 
-
+/*Размеры  290 * 117 */
 
     /*creating start game object*/
 var asteroid = new Image()
 asteroid.src='img/tema/astero.png'
 var user_board =new Image();
-user_board.src='img/User_Ships/1.png'
+user_board.src='img/User_Ships/13B.png'
 var alien_board =new Image();
-alien_board.src='img/Alien_Ships/Alien-Scout.png'
+alien_board.src='img/Alien_Ships/Alien-Bomber.png'
 /*вызов игры*/
-game()&&&&&
+
+
 
 /*-------------------*/
 var requestAnimFrame = (function(){
@@ -39,6 +40,8 @@ function update(){
 }
 
 function render(){
-contex.drawImage(user_board,0,0,44500,54400);
-contex.drawImage(asteroid,70,19,50,50);
+contex.drawImage(user_board,1,1,10,10);
+contex.drawImage(asteroid,290,117,10,10);
+    contex.drawImage(alien_board,150,50,10,10);
 }
+game();
