380cc08f649153314d09e0f7a1d919f59c164c99
Anikald <t1kend.offical@gmail.com>
2021-04-30 21:18:04 +1000

    оформил уничтожение объетов, систему жизни и очков, но их пока не видно

--- js/game.js
+++ js/game.js
@@ -16,9 +16,11 @@ var height_game = canvas.height - 150
 /*!Creating start game objects and their Function!*/
 
 /*--------------------------------------*/
+var flag = 1
 var asteroid = new Image()
 asteroid.src = 'img/tema/astero.png'
 var aster = []
+var SCORE = 0;
 var user_board = new Image()
 user_board.src = 'img/User_Ships/3B.png'
 var alien_board1 = new Image()
@@ -30,6 +32,7 @@ bullet_a.src = 'img/tema/fire_ul.png'
 var bul = [];
 var bul_a = [];
 var alien_board = [];
+var start = new Date().getTime()
 
 class Alien {
 
@@ -39,8 +42,13 @@ class Alien {
         this.damage = 70
         this.x = widht_game + 100;
         this.y = Math.random() * height_game;
-        this.dx = Math.random() + 0.2;
-        this.dy = Math.random() + 0.2;
+        this.dx = Math.random() * 2 + 1;
+        if (Math.random() !== 0) {
+            this.dy = -(Math.random() + 0.2)
+        } else {
+            this.dy = (Math.random() + 0.2)
+        }
+
         /* console.log(this)*/
     }
 
@@ -68,7 +76,11 @@ class Asteroid {
         this.x = widht_game + 100;
         this.y = Math.random() * height_game;
         this.dx = Math.random() + 0.5;
-        this.dy = Math.random() + 0.2;
+        if (Math.random() === 0) {
+            this.dy = -(Math.random() + 0.2)
+        } else {
+            this.dy = (Math.random() + 0.2)
+        }
         /* console.log(this)*/
     }
 
@@ -91,6 +103,7 @@ class User_ship {
         this.x = widht_game / 5;
         this.y = height_game / 2;
         this.health = 100
+        this.live = 3
     }
 
     render() {
@@ -127,7 +140,7 @@ class Bullet_A {
         this.y1 = al.y
         this.x2 = al.x + 40
         this.y2 = al.y + 35
-        this.dx = 0.1
+        this.dx = 0.4
 
     }
 
@@ -155,8 +168,13 @@ var requestAnimFrame = (function () {
             window.setTimeout(callback, 1000 / 20);
         };
 })();
-
-function ship_movement() {
+function del_all(){
+    bul_a.splice(0,bul_a.length)
+    aster.splice(0,aster.length)
+    alien_board.splice(0,alien_board.length)
+    contex.clearRect(0, 0, widht_game + 100, height_game + 200);
+}
+function ship_movement(handle) {
 
     ship.render()
     canvas.addEventListener("mousemove", function (event) {
@@ -164,21 +182,95 @@ function ship_movement() {
         ship.y = event.offsetY;
     })
 
-    if (time_bul % 55 === 0) {
+    if (time_bul % 60 === 0) {
         bul.push(new Bullet_U)
+    }
+
+    var f_d = 1;
+    var end = new Date().getTime() - start;
+    for (let j = 0; j < bul_a.length; j++) {
+        end = new Date().getTime() - start;
+        if (bul_a[j] !== undefined) {
+            if (f_d === 1) {
+                if (Math.abs(ship.y + 35 - bul_a[j].y1 + 13) < 70
+                    && Math.abs(ship.x - bul_a[j].x1) < 15
+                    || Math.abs(ship.y + 35 - bul_a[j].y2 + 13) < 70
+                    && Math.abs(ship.x - bul_a[j].x2) < 15) {
+                    console.log(end)
+                    ship.health -= bul_a[j].damage;
+                    f_d = 0;
+                   del_all();
+                    start = new Date().getTime()
+                    break;
+                }
+            }
+
+            if (end % 3000000000 === 0) {
+                f_d = 1
+            }
+        }
+    }
+    for (var j = 0; j < alien_board.length; j++) {
+        end = new Date().getTime() - start;
+        if (alien_board[j] !== undefined) {
+            if (f_d === 1) {
+                if (Math.abs(ship.y + 35 - alien_board[j].y + 35) < 70
+                    && Math.abs(ship.x - alien_board[j].x) < 35){
+                    console.log(end)
+                    ship.health -= alien_board[j].damage;
+                    f_d = 0;del_all();
+
+                    start = new Date().getTime()
+                    break;
+                }
+            }
+
+            if (end % 3000000000 === 0) {
+                f_d = 1
+            }
+        }
+    }
+    for (var j = 0; j < aster.length; j++) {
+        end = new Date().getTime() - start;
+        if (aster[j] !== undefined) {
+            if (f_d === 1) {
+                if (Math.abs(ship.y + 35 - aster[j].y + 25) < 70
+                    && Math.abs(ship.x - aster[j].x) < 35){
+                    console.log(end)
+                    ship.health -= aster[j].damage;
+                    f_d = 0;del_all()
+                    start = new Date().getTime()
+                    break;
+                }
+            }
+
+            if (end % 3000000000 === 0) {
+                f_d = 1
+            }
+        }
+    }
+    if (ship.health <= 0) {
+        ship.live--;
+        if (ship.live <= 0) {
+            flag = 0
+            console.log("GAMEOVER")
+        } else {
+            ship.health = 100;
+            user_board.globalAlpha = 0.5
+        }
     }
     time_bul++;
     for (let i = 0; i < bul.length; i++) {
         bul[i].render()
         bul[i].update()
-        if (bul[i].x >widht_game+1) {
+        if (bul[i].x > widht_game + 1) {
             bul.splice(i, 1)
         }
     }
 }
 
 function spawn_alien_ships() {
-    if (time_set_alien % 70 === 0) {
+    if (time_set_alien % 40 === 0) {
         alien_board.push(new Alien());
     }
     time_set_alien++;
@@ -187,7 +279,8 @@ function spawn_alien_ships() {
 
         alien_board[i].render()
         alien_board[i].update()
-        if (time_bul_a % 320 === 1) {
+
+        if (time_bul_a % 100 === 0) {
             bul_a.push(new Bullet_A(alien_board[i]))
         }
         time_bul_a++;
@@ -198,7 +291,23 @@ function spawn_alien_ships() {
                 bul_a.splice(j, 1)
             }
         }
-        if (alien_board[i].x < -84) {
+        for (let j = 0; j < bul.length; j++) {
+            if (alien_board[i] !== undefined) {
+                if (Math.abs(alien_board[i].y + 35 - bul[j].y1 + 13) < 70
+                    && Math.abs(alien_board[i].x - bul[j].x1) < 35
+                    || Math.abs(alien_board[i].y + 35 - bul[j].y2 + 13) < 100
+                    && Math.abs(alien_board[i].x - bul[j].x2) < 35) {
+                    alien_board[i].del = 1
+                    SCORE++;
+                    bul.splice(j, 1)
+                    break/* перепроверить */
+                }
+                if (alien_board[i].del === 1) {
+                    alien_board.splice(i, 1)
+                }
+            }
+        }
+        if (alien_board[i] !== undefined && alien_board[i].x < -84) {
             alien_board.splice(i, 1)
         }
 
@@ -217,17 +326,22 @@ function spawn_aster() {
         aster[i].render()
         aster[i].update()
         for (let j = 0; j < bul.length; j++) {
-            if (Math.abs(aster[i].y - 35 - bul[j].y1 - 13) < 70 && Math.abs(aster[i].x - bul[j].x1) < 35 ||
-              Math.abs(aster[i].y - 35 - bul[j].y2 - 13) < 100 && Math.abs(aster[i].x - bul[j].x2) < 35){
-                aster[i].del = 1
-                bul.splice(j, 1)
-                break/* перепроверить */
-            }
-            if (aster[i].del===1){
-                aster.splice(i, 1)
+            if (aster[i] !== undefined) {
+                if (Math.abs(aster[i].y + 35 - bul[j].y1 + 13) < 70
+                    && Math.abs(aster[i].x - bul[j].x1) < 35
+                    || Math.abs(aster[i].y + 35 - bul[j].y2 + 13) < 100
+                    && Math.abs(aster[i].x - bul[j].x2) < 35) {
+                    aster[i].del = 1
+                    SCORE++;
+                    bul.splice(j, 1)
+                    break/* перепроверить */
+                }
+                if (aster[i].del === 1) {
+                    aster.splice(i, 1)
+                }
             }
         }
-        if (aster[i].x < -84) {
+        if (aster[i] !== undefined && aster[i].x < -84) {
             aster.splice(i, 1)/* перепроверить */
         }
 
@@ -242,12 +356,14 @@ function spawn_objects() {
 }
 
 function game() {
-    contex.clearRect(0, 0, widht_game + 100, height_game + 100);
+    contex.clearRect(0, 0, widht_game + 100, height_game + 200);
     render();
 
-
-    requestAnimFrame(game);
-
+    if (flag === 1) {
+        requestAnimFrame(game);
+    } else {
+        console.log('GROEPWRPO')
+    }
 }
 
 function render() {
